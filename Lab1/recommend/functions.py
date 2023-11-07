73d129a1db0cb2bcc9b8cea7ddf65de5e791a7fc
Rongjian Gu <1846868573@qq.com>
2023-11-07 21:30:43 +0800

    Lab1.2 movie_recommendation complete

---
+++ Lab1/recommend/functions.py
@@ -0,0 +1,70 @@
+import os
+import random
+import pandas as pd
+import numpy as np
+
+
+def load_data_from_path(data_path, embedding, cache_dir):
+    cache_path = os.path.join(cache_dir, "ratings_matrix_cache")
+
+    # 加载数据集
+    if os.path.exists(cache_path):
+        ratings_matrix = pd.read_pickle(cache_path)
+    else:
+        ratings = pd.read_csv(data_path, dtype={
+            "User": np.int32, "Book": np.int32, "Rate": np.float32
+        }, usecols=range(3))
+        length = len(ratings)
+        for row in range(0, length):
+            if embedding[row] == 1:
+                ratings.at[row, "Rate"] = None
+        ratings_matrix = pd.pivot_table(
+            data=ratings, index=["User"], columns=["Book"], values="Rate")
+        ratings_matrix.to_pickle(cache_path)
+
+    return ratings_matrix
+
+def pearson_coefficient(rating_matrix, cache_dir, based="user"):
+    user_cache_path = os.path.join(cache_dir, "user_cache")
+    item_cache_path = os.path.join(cache_dir, "item_cache")
+    if based == "user":
+        if os.path.exists(user_cache_path):
+            similarity = pd.read_pickle(user_cache_path)
+        else:
+            similarity = rating_matrix.T.corr()
+            similarity.to_pickle(user_cache_path)
+    elif based == "item":
+        if(os.path.exists(item_cache_path)):
+            similarity = pd.read_pickle(item_cache_path)
+        else:
+            similarity = rating_matrix.corr()
+            similarity.to_pickle(item_cache_path)
+    else:
+        print("base value error!")
+        exit(0)
+    return similarity
+
+def predict_score(user_id, item_id, rating_matrix, user_similarity_matrix):
+    # 寻找相关的用户
+    relevant_users = user_similarity_matrix[user_id].drop([user_id]).dropna()
+    relevant_users = relevant_users.where(relevant_users > 0).dropna()
+    if relevant_users.empty is True: # 没有相关的用户，直接返回平均值
+        return 2.5
+    # 寻找跟item_id相关的用户
+    ids = set(rating_matrix[item_id].dropna().index) & set(relevant_users.index)
+    relevant_users = relevant_users.loc[list(ids)]
+    # 预测评分
+    numerator = 0
+    denominator = 0
+    for uid in relevant_users.index:
+        similarity = relevant_users[uid]
+        item_rating = rating_matrix.loc[uid].dropna()[item_id]
+        numerator += similarity * item_rating
+        denominator += similarity
+
+    final_rating = numerator / denominator
+    return round(final_rating, 2)
+
+
+
+
---
+++ Lab1/recommend/movie_recommendation.py
@@ -0,0 +1,43 @@
+import random
+
+import pandas as pd
+import numpy as np
+import functions
+
+data_path = "./data/movie_score.csv"
+cache_dir = "./data/movie_cache/"
+
+embedding = [0]
+file = pd.read_csv(data_path)
+for i in range(1, len(file)):
+    embedding.append(1 - embedding[i - 1])
+random.seed(0)
+random.shuffle(embedding)
+
+rating_matrix = functions.load_data_from_path(data_path, cache_dir=cache_dir, embedding=embedding)
+user_similarity = functions.pearson_coefficient(rating_matrix, cache_dir, based="user")
+item_similarity = functions.pearson_coefficient(rating_matrix, cache_dir, based="item")
+
+rating = pd.read_csv(data_path, dtype={
+    "User": np.int32, "Movie": np.int32, "Rate": np.float32}, usecols=range(3))
+rating_matrix_2 = pd.pivot_table(data=rating, index=["User"], columns=["Movie"], values="Rate")
+
+
+var_predict = 0
+var_random_guess = 0
+cnt = 0
+for row in range(0, len(rating)):
+    if embedding[row] == 1:
+        predict_rating = functions.predict_score(rating.at[row, "User"], rating.at[row, "Movie"], rating_matrix, user_similarity)
+        random_guess_rating = random.randint(0, 6)
+        real_rating = rating.at[row, "Rate"]
+        cnt += 1
+        var_predict += (predict_rating - real_rating) ** 2
+        var_random_guess += (random_guess_rating - real_rating) ** 2
+        if (cnt % 10 == 0):
+            print(cnt, var_predict / cnt, var_random_guess / cnt)
+var_predict = var_predict / cnt
+var_random_guess = var_random_guess / cnt
+print(var_predict, var_random_guess)
+
+
